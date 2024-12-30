931da59d8533cfae303db4a379d849b75abc0b8b
SMSBombdev <sultansariyildiz4@gmail.com>
2024-12-30 06:51:39 -1000

    SmsBomb.py

--- enough.py
+++ enough.py
@@ -24,7 +24,7 @@ while 1:
     Yapımcı: Muhammed
 
 
-    Başladığında kapatmak için: CTRL + C           {}Yapan {}@RivenShadow\n  
+    Başladığında kapatmak için: CTRL + C     {}           {}By {}@RivenShadow\n  
     """.format(Fore.LIGHTCYAN_EX, len(servisler_sms), Style.RESET_ALL, Fore.LIGHTRED_EX))
     try:
         menu = (input(Fore.LIGHTMAGENTA_EX + " 1- SMS Gönder (Normal)\n\n 2- SMS Gönder (Hızlı)\n\n 3- Çıkış\n\n" + Fore.LIGHTYELLOW_EX + " Seçtiğin: "))
